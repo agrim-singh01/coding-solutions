f69e4cdc64602648f7cfe159f7c6cd713c98775b
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-04 19:13:13 +0530

    ✅ Find Minimum in Rotated Sorted Array | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.9 MB | 2026-09-04

---
+++ leetcode/medium/0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        //Actual Logic
+        while(low <= high){
+            if(nums[low] <= nums[high]){
+                return nums[low];
+            }
+            int mid = low + (high - low)/2;
+
+            if(nums[low] <= nums[mid]){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
