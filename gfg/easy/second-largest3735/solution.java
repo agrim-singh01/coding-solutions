cae51a14fda93a87b762eab77f615fe1754ddb25
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-05 02:04:19 +0530

    ✅ Second Largest | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-04

---
+++ gfg/easy/second-largest3735/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+       
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > largest){
+                second = largest;
+                largest = arr[i];
+            }else if(arr[i] > second && arr[i] < largest){
+                second = arr[i];
+            }
+        }
+       
+        //
+        if(second == Integer.MIN_VALUE){
+            return -1;
+        }
+         return second;
+        
+    }
+}
