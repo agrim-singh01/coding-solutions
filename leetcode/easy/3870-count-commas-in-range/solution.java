661f7f7bdc5124c83f9e10f8ae48180e575c595c
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-08 19:47:27 +0530

    ✅ Count Commas in Range | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.7 MB | 2026-09-08

--- leetcode/easy/3870-count-commas-in-range/solution.java
+++ leetcode/easy/3870-count-commas-in-range/solution.java
@@ -1,12 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-     if(n > 999){
-        return 3;
-     }
-     if(n<=1000 || n>=100000){
-
-     return 0;
-    }
-    return 6;
+        if(n > 999){
+            return n-999;
+        }
+        return 0;
     }
 }
