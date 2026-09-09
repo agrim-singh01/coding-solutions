b61c254226bff3dd90c84e7a07bfed4010aef08d
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-09 23:15:19 +0530

    ✅ Exit Point in a Matrix | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- gfg/medium/exit-point-in-a-matrix0905/solution.java
+++ gfg/medium/exit-point-in-a-matrix0905/solution.java
@@ -1,34 +1,36 @@
-import java.util.*;
 class Solution {
     public List<Integer> exitPoint(int[][] mat) {
-        List<Integer> ans = new ArrayList<>();
-        int i=0;
-        int j=0;
-        int direction = 0;
+      List<Integer> ans = new ArrayList<>();
+      int i = 0;
+      int j = 0;
+      int direction = 0;
+      
+      int prevrow = 0;
+      int prevcol = 0;
+      
+      while(i>=0 && i < mat.length && j >=0 && j < mat[0].length){
+          prevrow = i;
+          prevcol = j;
+          
+          if(mat[i][j] == 1){
+              direction =(direction+1)%4;
+              mat[i][j] = 0;
+          }
+          if(direction == 0){
+              j++;
+          }else if(direction == 1){
+              i++;
+          }else if(direction == 2){
+              j--;
+          }else{
+              i--;
+          }
+      }
+      ans.add(prevrow);
+      ans.add(prevcol);
+      
+      return ans;
+      
         
-        int prevrow = 0;
-        int prevcol = 0;
-        
-        while(i >= 0 && i < mat.length && j >= 0 && j < mat[0].length){
-            prevrow = i;
-            prevcol = j;
-            
-            if(mat[i][j] == 1){
-                direction = (direction + 1) % 4;
-                mat[i][j] = 0;
-            } if(direction == 0){
-                j++;
-                }else if(direction == 1){
-                    i++;
-                }else if(direction == 2){
-                    j--;
-                }else{
-                    i--;
-                }
-        }
-        ans.add(prevrow);
-        ans.add(prevcol);
-        
-        return ans;
     }
 }
