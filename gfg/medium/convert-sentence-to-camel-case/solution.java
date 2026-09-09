6e4c54d6dcd9331820cebae35917ebe3e0963f76
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-10 02:14:49 +0530

    ✅ Convert Sentence to Camel Case | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- gfg/medium/convert-sentence-to-camel-case/solution.java
+++ gfg/medium/convert-sentence-to-camel-case/solution.java
@@ -1,24 +1,23 @@
 class Solution {
-    // Function to convert the given string to Camel Case
-    String camelCase(String s){
+    String toCamelCase(String s){
         StringBuilder ans = new StringBuilder();
-        boolean makeupper = false;
-        
+        boolean upper = false;
         for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch == ' ' ){
-                makeupper = true;
-            }else if(makeupper == true){
+            if(ch == ' '){
+                upper = true;
+            }else if(upper == true){
                 ans.append(Character.toUpperCase(ch));
-                makeupper=false;
+                upper = false;
             }else{
                 ans.append(ch);
             }
         }
         return ans.toString();
-        
     }
+   
     public String convertToCamelCase(String s) {
-    return camelCase(s); 
+        return toCamelCase(s);
+        
     }
 }
