73a6181c856fbddea8d9490dba736212807050f0
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-10 01:52:41 +0530

    ✅ Check if the Sentence Is Pangram | Easy | Java | Runtime: 2 ms (62.89%) | Memory: 43 MB | 2026-09-09

--- leetcode/easy/1832-check-if-the-sentence-is-pangram/solution.java
+++ leetcode/easy/1832-check-if-the-sentence-is-pangram/solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean size[] = new boolean[26];
-       for(int i=0;i<sentence.length();i++){
-         char letter = sentence.charAt(i);
-         int character = letter - 'a';
-         size[character] = true;
-       }
-       for(int i=0;i<26;i++){
-  
-        if(size[i] == false){
-            return false;
+        boolean[] size = new boolean[26];
+        for(int i= 0;i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            int character = ch - 'a';
+            size[character] = true;
         }
-       }
-       return true;
+        for(int i=0;i<26;i++){
+            if(size[i] == false){
+return false;
+            }
+        }
+        return true;
     }
 }
