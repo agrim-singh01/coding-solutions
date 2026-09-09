73b9481a824f2b28edea219b4e128aa3a38216f4
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-09 22:05:01 +0530

    ✅ Rotate Image | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.9 MB | 2026-09-09

---
+++ leetcode/medium/0048-rotate-image/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        
+
+        for(int i=0;i<row;i++){
+            for(int j=i;j<column;j++){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0;i<matrix.length;i++){
+            int left = 0;
+            int right = matrix[i].length - 1;
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+
+       
+    }
+}
