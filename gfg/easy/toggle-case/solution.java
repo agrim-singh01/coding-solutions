d1019412fdc45c9b114abafe984de267b9d92777
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-10 01:43:45 +0530

    ✅ Toggle Case | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- gfg/easy/toggle-case/solution.java
+++ gfg/easy/toggle-case/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public String toggleCase(String s) {
-       StringBuilder ans = new StringBuilder();
-       for(int i = 0;i<s.length();i++){
-           char ch = s.charAt(i);
-           if(Character.isUpperCase(ch)){
-               ans.append(Character.toLowerCase(ch));
-           }else{
-               ans.append(Character.toUpperCase(ch));
-           }
-       }
-       return ans.toString();
-        
+      StringBuilder ans = new StringBuilder();
+      
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch)){
+                ans.append(Character.toUpperCase(ch));
+            }else{
+                ans.append(Character.toLowerCase(ch));
+            }
+        }
+        return ans.toString();
     }
 }
