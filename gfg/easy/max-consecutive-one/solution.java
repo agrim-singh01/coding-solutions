6690fa7442c7651356c1ab9a81e5aacdda1ffa68
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-10 08:32:39 +0530

    ✅ Max Consecutive Bit | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ gfg/easy/max-consecutive-one/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxConsecBits(int[] arr) {
+        // code here
+        int count = 1;
+        int max = 1;
+        for(int i = 1;i<arr.length;i++){
+            if(arr[i] == arr[i-1]){
+                count++;
+            }else{
+                count = 1;
+            }
+            if(max<count){
+                max = count;
+            }
+        }
+        return max;
+    }
+}
