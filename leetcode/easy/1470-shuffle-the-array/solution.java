695b9b7738464814233ba8ca654177636e757e64
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-10 10:26:29 +0530

    ✅ Shuffle the Array | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 46.3 MB | 2026-09-10

---
+++ leetcode/easy/1470-shuffle-the-array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        int[] ans = new int[nums.length];
+
+        int i = 0;
+        int j = n;
+        int k = 0;
+
+        while (i < n) {
+
+            ans[k] = nums[i];
+            k++;
+
+            ans[k] = nums[j];
+            k++;
+
+            i++;
+            j++;
+        }
+
+        return ans;
+    }
+}
