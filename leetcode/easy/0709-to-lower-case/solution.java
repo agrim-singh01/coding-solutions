796549275d24a469e87d44ff432ee374180f9e79
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-11 09:44:11 +0530

    ✅ To Lower Case | Easy | Java | Runtime: 1 ms (25.97%) | Memory: 43.3 MB | 2026-09-11

---
+++ leetcode/easy/0709-to-lower-case/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toLowerCase(String s) {
+       StringBuilder sb = new StringBuilder();
+       for(int i=0;i<s.length();i++){
+        char ch = s.charAt(i);
+        if(Character.isUpperCase(ch)){
+            sb.append(Character.toLowerCase(ch));
+        }else{
+            sb.append(ch);
+        }
+       }
+       return sb.toString();
+           }
+}
