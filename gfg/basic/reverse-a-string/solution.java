907ef095760686059ef0fcd9ba38eabd48775e3a
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-11 11:01:46 +0530

    ✅ Reverse a String | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ gfg/basic/reverse-a-string/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public static String reverseString(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        return sb.toString();
+        
+    }
+}
