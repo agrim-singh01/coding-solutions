d883098d8c381e9c542000531a311aa764fb7184
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-11 11:07:15 +0530

    ✅ Palindrome String | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ gfg/easy/palindrome-string0817/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s);
+         sb.reverse();
+         String ans = sb.toString();
+         
+         return ans.equals(s);
+    
+        
+    }
+}
