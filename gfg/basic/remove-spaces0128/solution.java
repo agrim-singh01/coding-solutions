a337a9c503dea879af0271d99398b3336549480d
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-11 11:17:01 +0530

    ✅ Remove Spaces | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ gfg/basic/remove-spaces0128/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeSpaces(String s) {
+        
+        
+        //StringBuilder sb = new StringBuilder(s);
+
+         return   s.replace(" ","");
+        
+    
+    
+        
+    }
+}
