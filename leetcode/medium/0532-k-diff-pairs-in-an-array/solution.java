ed42b49524829cc50f96e89a7e442f21b149424c
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-12 08:52:55 +0530

    ✅ K-diff Pairs in an Array | Medium | Java | Runtime: 10 ms (90.47%) | Memory: 46.4 MB | 2026-09-12

---
+++ leetcode/medium/0532-k-diff-pairs-in-an-array/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 1;
+        int count = 0;
+
+        while(right < nums.length){
+            if(left == right){
+                right++;
+                continue;
+            }
+            int diff = nums[right] - nums[left];
+            if(diff > k){
+left++;
+            }else if(diff < k){
+                right++;
+            }else{
+                count++;
+
+                int prevLeft = nums[left];
+                int prevRight = nums[right];
+
+                while(left<nums.length && nums[left] == prevLeft){
+                    left++;
+                }
+                while(right<nums.length && nums[right] == prevRight){
+                    right++;
+                }
+            }
+        }
+        return count;
+    }
+}
