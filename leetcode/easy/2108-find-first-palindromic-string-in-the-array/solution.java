a2e783b07d6529732c7994c9a260e41a64d38944
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-13 19:27:26 +0530

    ✅ Find First Palindromic String in the Array | Easy | Java | Runtime: 4 ms (27.33%) | Memory: 47.6 MB | 2026-09-13

---
+++ leetcode/easy/2108-find-first-palindromic-string-in-the-array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+     for(int i=0;i<words.length;i++){
+         StringBuilder sb = new StringBuilder(words[i]);
+         sb.reverse();
+         String palin = sb.toString();
+         if(words[i].equals(palin)){
+            return words[i];
+         }
+     } 
+     return "";
+    }
+}
