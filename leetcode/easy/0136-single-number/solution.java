3624d62dd692816de1c8a1ca42c38206cc20b67b
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-13 18:00:26 +0530

    ✅ Single Number | Easy | Java | Runtime: 1 ms (99.94%) | Memory: 46.9 MB | 2026-09-13

--- leetcode/easy/0136-single-number/solution.java
+++ leetcode/easy/0136-single-number/solution.java
@@ -1,22 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-
-    
-        if (n == 1) return nums[0];
-
-       
-        if (nums[0] != nums[1]) return nums[0];
-
-       
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
+        // Arrays.sort(nums);
+        // int n = nums.length;
+         // if (n == 1) return nums[0];
+        // if (nums[0] != nums[1]) return nums[0];
+        // for (int i = 1; i < n - 1; i++) {
+        //     if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[n - 1];
+        int unique = 0;
+        for(int i=0;i<nums.length;i++){
+            unique ^= nums[i];
         }
-
-      
-        return nums[n - 1];
+        return unique;
+        
     }
 }
