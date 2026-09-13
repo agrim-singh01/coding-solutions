cfd8b8cd933487459abd28453a064c1e950c2616
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-13 10:00:06 +0530

    ✅ Sum Multiples | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-09-13

---
+++ leetcode/easy/2652-sum-multiples/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum1 = 0,sum2=0,sum3=0;
+        for(int i=1;i<=n;i++){
+            if(i % 3 == 0){
+                sum1 += i;
+            }else if(i % 5 == 0){
+                sum2 += i;
+            }else if(i % 7 == 0){
+                sum3 += i;
+            }
+
+        }
+        return sum1+sum2+sum3;
+    }
+}
