8095853beee57f1635949baa4b893a851289e9d7
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-14 20:45:25 +0530

    ✅ Rotate Array | Medium | Java | Runtime: 4 ms (48.95%) | Memory: 268.5 MB | 2026-09-14

--- leetcode/medium/0189-rotate-array/solution.java
+++ leetcode/medium/0189-rotate-array/solution.java
@@ -13,15 +13,36 @@ class Solution {
     ////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////////
     //Method-2-Auxillary array placement
+    // int n = nums.length;
+    // int[] tempArray = new int[n];
+    // for(int i=0;i<n;i++){
+    //     int futureindex = (i+k)%n;
+    //     tempArray[futureindex] = nums[i];
+    // }
+    // for(int i=0;i<n;i++){
+    //     nums[i] = tempArray[i];
+    // }
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    //Method-3:-The reversal Algorithm
     int n = nums.length;
-    int[] tempArray = new int[n];
-    for(int i=0;i<n;i++){
-        int futureindex = (i+k)%n;
-        tempArray[futureindex] = nums[i];
+    if (nums == null || nums.length == 0) {
+            return;
+        }
+    k = k % n;
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
     }
-    for(int i=0;i<n;i++){
-        nums[i] = tempArray[i];
+
+    private void reverse(int[] nums,int start,int end){
+        while(start<end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end]=  temp;
+        start++;
+        end--;
+        }
     }
-    
     }
-}
