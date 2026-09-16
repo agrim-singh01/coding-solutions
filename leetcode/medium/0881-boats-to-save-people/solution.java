579b1173d5b937b712c447cff3001d0c1cd387e9
Agrim Singh <agrim.singh.cse@gmail.com>
2026-09-16 09:48:55 +0530

    ✅ Boats to Save People | Medium | Java | Runtime: 19 ms (98.88%) | Memory: 56.2 MB | 2026-09-16

---
+++ leetcode/medium/0881-boats-to-save-people/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left =0;
+        int n = people.length;
+      int boat =0;
+        int right = people.length-1;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                left++;
+                right--;
+            }else{
+                right--;
+                
+            }
+            boat++;
+        }
+        return boat;
+    }
+}
